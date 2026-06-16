3d72b246f1d885439c96147ff52f4da5d6dd2183
Nico Ritschel <nico@elsl.de>
2026-06-16 17:10:20 -0400

    update demo question

---
+++ questions/analogCircuitExample/server.py
@@ -0,0 +1,64 @@
+import random
+
+
+def generate(data):
+    data["params"]["readme_circuit"] = {
+        "circuit": {
+            "components": [
+                {"from": "0,0", "to": "0,4", "type": "SourceV", "label": "$V_S$"},
+                {"from": "0,4", "to": "3,4", "type": "Resistor", "label": "$R_1$"},
+                {"from": "3,4", "to": "3,0", "type": "Resistor", "label": "$R_2$"},
+                {"from": "3,0", "to": "0,0"},
+                {"pos": "0,0", "type": "Ground"},
+            ],
+            "nodes": [
+                {"pos": "0,4", "dot": "filled", "label": "a"},
+            ],
+            "annotations": [
+                {"type": "LoopCurrent", "bounds": ["0,0", "3,4"], "label": "$I$"},
+            ],
+        }
+    }
+
+    source_voltage = random.choice([6, 9, 12, 15])
+    r1 = random.choice([1000, 1500, 2200, 3300, 4700])
+    r2 = random.choice([1000, 1500, 2200, 3300, 4700])
+    vout = source_voltage * r2 / (r1 + r2)
+
+    data["params"]["random_circuit"] = {
+        "circuit": {
+            "components": [
+                {
+                    "from": "0,0",
+                    "to": "0,3",
+                    "type": "SourceV",
+                    "label": f"${source_voltage}\\,V$",
+                },
+                {
+                    "from": "0,3",
+                    "to": "3,3",
+                    "type": "Resistor",
+                    "label": f"$R_1={r1 / 1000:g}\\,k\\Omega$",
+                },
+                {
+                    "from": "3,3",
+                    "to": "3,0",
+                    "type": "Resistor",
+                    "label": [
+                        f"$R_2={r2 / 1000:g}\\,k\\Omega$",
+                        {"label": ["+", "$V_{out}$", "-"], "loc": "bottom"},
+                    ],
+                },
+                {"from": "3,0", "to": "0,0"},
+                {"pos": "0,0", "type": "Ground"},
+            ],
+            "nodes": [
+                {"pos": "3,3", "dot": "filled", "label": "out"},
+            ],
+            "annotations": [
+                {"type": "LoopCurrent", "bounds": ["0,0", "3,3"], "label": "$I$"},
+            ],
+        }
+    }
+
+    data["correct_answers"]["vout"] = vout
